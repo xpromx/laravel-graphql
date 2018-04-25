98a441857c0abe96d3bc94798b71d588387e0ed1
xpromx <xpromx@gmail.com>
2018-04-25 09:25:36 +0900

    optional next_page & prev_page fix

--- src/Type/ConnectionType.php
+++ src/Type/ConnectionType.php
@@ -6,43 +6,37 @@ use GraphQL\Type\Definition\ObjectType;
 use Xpromx\GraphQL\Definition\Type;
 use GraphQL;
 
-class ConnectionType extends ObjectType {
-
+class ConnectionType extends ObjectType
+{
     public $type = false;
 
-    public function __construct( $typeName )
+    public function __construct($typeName)
     {
         $this->type = $typeName;
         
         $config = [
 
-            'name'  => str_plural( $typeName ) . 'Connection',
+            'name'  => str_plural($typeName) . 'Connection',
             'model' => GraphQL::type($typeName)->config['model'],
 
             'fields' => [
 
                 'nodes' => [
-                    'type' => Type::listOf( GraphQL::type($typeName) ),
-                    'resolve' => function( $root ){
-                        
+                    'type' => Type::listOf(GraphQL::type($typeName)),
+                    'resolve' => function ($root) {
                         return $root;
-
                     },
                 ],
 
                 'pageInfo' => [
                     'type' => GraphQL::type('PageInfo'),
-                    'resolve' => function( $root, $args, $context ){
-
+                    'resolve' => function ($root, $args, $context) {
                         $pagination = $root->toArray();
 
-                        if( $pagination['next_page_url'] )
-                        {
+                        if (array_key_exists('next_page_url', $pagination)) {
                             $pagination['next_page'] = $pagination['current_page'] + 1;
                         }
-
-                        if( $pagination['prev_page_url'] )
-                        {
+                        if (array_key_exists('prev_page_url', $pagination)) {
                             $pagination['prev_page'] = $pagination['current_page'] - 1;
                         }
 
@@ -55,7 +49,5 @@ class ConnectionType extends ObjectType {
         ];
 
         parent::__construct($config);
-
     }
-
 }
