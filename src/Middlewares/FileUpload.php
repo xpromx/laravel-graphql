0f6575f68e6db4ada16fa4549f2d1ffc24e3ec7c
xpromx <xpromx@gmail.com>
2018-05-02 21:18:19 +0900

    FileUpload Middleware

--- src/Middlewares/FileUpload.php
+++ src/Middlewares/FileUpload.php
@@ -15,36 +15,36 @@ class FileUpload
     public function handle($request, Closure $next)
     {
         $contentType = $request->header('content-type') ?? '';
+
         if (mb_stripos($contentType, 'multipart/form-data') !== false) {
             $this->validateRequest($request);
             $operations = json_decode($request->get('operations'), true);
-            $map = json_decode($request->get('map'), true);
-            $files = $request->file();
+            $files = json_decode($request->get('files'), true);
+
             if (isset($operations['operationName'])) {
                 $operations['operation'] = $operations['operationName'];
                 unset($operations['operationName']);
             }
-            foreach ($map as $fileKey => $locations) {
-                foreach ($locations as $location) {
-                    $key = array_last(explode('.', $location));
-                    $file = $request->file($key);
-                    $operations['variables'][$key] = [
-                      'available' => true
-                    ];
-                }
+
+            foreach ($files as $key) {
+                $file = $request->file($key);
+                $operations['variables'][$key] = $file;
             }
+            
             $request->headers->add(['content-type' => 'application/json']);
             $request->replace($operations);
         }
+
         return $next($request);
     }
+
     private function validateRequest($request)
     {
         if (!$request->has('operations')) {
             throw new \Exception('Request is expected to provide an operation');
         }
-        if (!$request->has('map')) {
-            throw new \Exception('Request is expected to provide a map');
+        if (!$request->has('files')) {
+            throw new \Exception('Request is expected to provide a files');
         }
     }
 }
